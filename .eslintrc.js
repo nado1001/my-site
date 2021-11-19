9dedb889c2c67f5ae4f7d269828d6a51a9cd4cf8
take-tech1001 <tk.tech1001@gmail.com>
2021-11-19 19:37:53 +0900

    🎉 ESlint Prettier設定

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  parser: "@typescript-eslint/parser",
+  parserOptions: { project: "./tsconfig.json" },
+  plugins: [
+    '@typescript-eslint',
+    "simple-import-sort", "import-access",
+  ],
+  extends: [
+    "eslint:recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:jsx-a11y/recommended",
+    "next/core-web-vitals",
+    "prettier"
+  ],
+  rules: {
+    "no-console": ["warn", { allow: ["warn", "info", "error"] }],
+    "no-restricted-syntax": ["error", { selector: "TSEnumDeclaration", message: "Don't declare enums" }],
+    "prefer-arrow-callback": "error",
+    "prefer-const": "error",
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-handler-names": [
+      "error",
+      {
+        eventHandlerPrefix: "handle",
+        eventHandlerPropPrefix: "on",
+        checkLocalVariables: true,
+        checkInlineFunction: true,
+      },
+    ],
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  singleQuote: true,
+  trailingComma: 'none',
+  semi: false,
+  jsxSingleQuote: false,
+  jsxBracketSameLine: false,
+  arrowParens: 'always',
+}
