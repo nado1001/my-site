c4a5e666412f1e8c9905803e80ee7405f7f73234
take-tech1001 <tk.tech1001@gmail.com>
2021-11-20 15:55:04 +0900

    ✨ Add custom page extensions

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  future: { strictPostcssConfiguration: true },
+  pageExtensions: ['page.tsx', 'page.ts'],
   reactStrictMode: true,
+  typescript: { ignoreBuildErrors: true },
+  poweredByHeader: false
 }
