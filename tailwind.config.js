78ddfab4682d17ffe8749687ce8342b8cfe47af4
take-tech1001 <tk.tech1001@gmail.com>
2021-11-21 13:16:13 +0900

    ✨ ダークモード対応

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,19 @@
 module.exports = {
   mode: 'jit',
   purge: ['./src/**/*.{ts,tsx}'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {}
   },
   variants: {
-    extend: {}
+    extend: {
+      colors: {
+        darkThema01: '#0F0F0F',
+        darkThema02: '#212121',
+        darkThema03: '#303133',
+        darkThema04: '#575757'
+      }
+    }
   },
   plugins: []
 }
