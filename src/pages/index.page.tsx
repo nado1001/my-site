93dce66ce477181ee6172bc34a07cff704c1f541
take-tech1001 <tk.tech1001@gmail.com>
2021-11-25 23:09:58 +0900

    🎨

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,7 +1,17 @@
 import type { NextPage } from 'next'
 
+import { Seo } from '../component/Seo'
+import { Layout } from '../layout'
+
 const Home: NextPage = () => {
-  return <></>
+  return (
+    <>
+      <Seo path="/" title="Home" description="Homeです" />
+      <Layout>
+        <div>home</div>
+      </Layout>
+    </>
+  )
 }
 
 export default Home
