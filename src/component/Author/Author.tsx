6c8e7e7f7d4f1883d1a429ba90d4b69ece6edad9
take-tech1001 <tk.tech1001@gmail.com>
2021-12-07 23:04:11 +0900

    ✨

---
+++ src/component/Author/Author.tsx
@@ -0,0 +1,16 @@
+// import Image from 'next/image'
+// import { useTheme } from 'next-themes'
+import type { VFC } from 'react'
+
+/**
+ * @package
+ */
+export const Author: VFC = () => {
+  // const { theme } = useTheme()
+
+  return (
+    <>
+      <p>aaa</p>
+    </>
+  )
+}
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next'
 
+import { Article } from '../component/Article'
+import { Author } from '../component/Author'
 import { Seo } from '../component/Seo'
 import { Layout } from '../layout'
 
@@ -8,7 +10,22 @@ const Home: NextPage = () => {
     <>
       <Seo path="/" title="Home" description="Homeです" />
       <Layout>
-        <div>home</div>
+        <div className="md:px-20 md:max-w-screen-lg md:mx-auto md:pb-12 md:pt-28 md:grid md:grid-cols-2 md:gap-[50px]">
+          <div className="md:max-w-[724px] md:col-span-2">
+            <h1 className="font-bold text-4xl sm:py-8 md:pb-7">Articles</h1>
+            <div className="grid lg:grid-cols-3 grid-cols-2 md:gap-[30px] sm:gap-4">
+              <Article />
+              <Article />
+              <Article />
+              <Article />
+              <Article />
+              <Article />
+            </div>
+          </div>
+          <div className="md:max-w-[250px] md:col-span-1">
+            <Author />
+          </div>
+        </div>
       </Layout>
     </>
   )
