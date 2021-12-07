67add235d4b7566190c6b7e1bf74c614767cd734
take-tech1001 <tk.tech1001@gmail.com>
2021-12-08 01:26:15 +0900

    ♻️

--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -10,8 +10,8 @@ const Home: NextPage = () => {
     <>
       <Seo path="/" title="Home" description="Homeです" />
       <Layout>
-        <div className="md:px-20 md:max-w-screen-lg md:mx-auto md:pb-12 md:pt-28 md:grid md:grid-cols-2 md:gap-[50px]">
-          <div className="md:max-w-[724px] md:col-span-2">
+        <div className="md:max-w-screen-lg md:mx-auto md:pb-12 md:pt-28 md:grid md:grid-cols-article md:gap-[50px]">
+          <div className="md:max-w-[724px] md:col-start-1 md:col-end-2 sm:mx-4">
             <h1 className="font-bold text-4xl sm:py-8 md:pb-7">Articles</h1>
             <div className="grid lg:grid-cols-3 grid-cols-2 md:gap-[30px] sm:gap-4">
               <Article />
@@ -22,7 +22,7 @@ const Home: NextPage = () => {
               <Article />
             </div>
           </div>
-          <div className="md:max-w-[250px] md:col-span-1">
+          <div className="md:max-w-[250px] md:col-start-2 md:col-end-3 md:pt-[68px] sm:mt-12">
             <Author />
           </div>
         </div>
