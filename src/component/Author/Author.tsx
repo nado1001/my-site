8037d5781bf9876a7409447758cb0e3f282d920e
take-tech1001 <tk.tech1001@gmail.com>
2021-12-08 01:25:51 +0900

    ✨ Authorコンポーネント追加

--- src/component/Author/Author.tsx
+++ src/component/Author/Author.tsx
@@ -1,16 +1,57 @@
-// import Image from 'next/image'
-// import { useTheme } from 'next-themes'
+import Image from 'next/image'
+import { useTheme } from 'next-themes'
 import type { VFC } from 'react'
 
 /**
  * @package
  */
 export const Author: VFC = () => {
-  // const { theme } = useTheme()
+  const { theme } = useTheme()
 
   return (
-    <>
-      <p>aaa</p>
-    </>
+    <div className="md:dark:bg-darkBg03 md:border sm:border-t dark:border-darkBorder01 md:rounded-lg px-5 md:py-10 sm:pt-8 sm:pb-28 shadow-md">
+      <div className="flex items-end">
+        <Image
+          src="/image/nado.svg"
+          width="74"
+          height="73"
+          alt="nado"
+          className=""
+        />
+        <div className="pl-4 pb-1">
+          <span className="block pb-2 font-bold">ナド</span>
+          <a
+            href="https://twitter.com/nado_b1ue"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:opacity-80"
+          >
+            <Image
+              src={`/image/twitter${theme === 'dark' ? '_dark' : ''}.svg`}
+              width="24"
+              height="22"
+              alt="twitter"
+              className=""
+            />
+          </a>
+          <a
+            href="https://github.com/take-tech1001"
+            target="_blank"
+            className="ml-2 inline-block hover:opacity-80"
+            rel="noreferrer"
+          >
+            <Image
+              src={`/image/github${theme === 'dark' ? '_dark' : ''}.svg`}
+              width="24"
+              height="22"
+              alt="github"
+            />
+          </a>
+        </div>
+      </div>
+      <p className="pt-6">
+        テキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキストテキスト
+      </p>
+    </div>
   )
 }
