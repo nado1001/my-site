be89ef757f20af0fab7462deaf0047d7d91cf7ec
take-tech1001 <tk.tech1001@gmail.com>
2021-12-18 16:43:21 +0900

    ✨ ArticleコンポーネントにAPI追加

--- src/component/Article/Article.tsx
+++ src/component/Article/Article.tsx
@@ -1,17 +1,34 @@
+import cc from 'classcat'
+import { format } from 'date-fns'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useTheme } from 'next-themes'
 import type { VFC } from 'react'
 
+type Props = {
+  id: string
+  tags: Array<string>
+  title: string
+  date: string
+  slug: string
+  length: number
+}
+
 /**
  * @package
  */
-export const Article: VFC = () => {
+export const Article: VFC<Props> = (props) => {
   const { theme } = useTheme()
 
   return (
-    <article className="bg-white dark:bg-darkBg03 dark:hover:bg-darkHoverBg01 hover:shadow-lg border dark:border-darkBorder01 border-border01 border-solid rounded-lg shadow-md">
-      <Link href="/articles/1">
+    <article
+      className={cc([
+        'bg-white dark:bg-darkBg03 dark:hover:bg-darkHoverBg01 hover:shadow-lg border dark:border-darkBorder01 border-border01 border-solid rounded-lg shadow-md md:min-h-[250px] sm:min-h-[186px]',
+        // TODO: 1記事以上になったら削除
+        { 'md:max-w-[308px]': props.length === 1 }
+      ])}
+    >
+      <Link href={`/articles/${props.slug}`}>
         <a className="block">
           <div className="dark:bg-darkBg04 bg-bg02 flex justify-center items-center sm:py-4 md:py-6 dark:border-b-0 border-b border-border01 rounded-t-[6px]">
             <Image
@@ -24,10 +41,10 @@ export const Article: VFC = () => {
             />
           </div>
           <div className="sm:p-[9px] md:px-2 md:py-7 text-[15px] rounded-b-[6px]">
-            <span className="block text-right text-xs">2021.11.16</span>
-            <h2 className="mt-2">
-              タイトルタイトルタイトルタイトルタイトルタイトルタイトルタイトル
-            </h2>
+            <span className="block text-right text-xs">
+              {format(new Date(props.date), 'yyyy.MM.dd')}
+            </span>
+            <h2 className="mt-2">{props.title}</h2>
           </div>
         </a>
       </Link>
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -1,28 +1,56 @@
-import type { NextPage } from 'next'
+// import { useQuery } from '@apollo/client'
+import cc from 'classcat'
+import type { GetStaticProps } from 'next'
+import type { VFC } from 'react'
 
+import { addApolloState, initializeApollo } from '../apollo/apolloClient'
+import type { GetPostsQuery } from '../apollo/graphql'
+import { GET_POSTS } from '../apollo/queries'
 import { Article } from '../component/Article'
 import { Author } from '../component/Author'
 import { Seo } from '../component/Seo'
 import { Layout } from '../layout'
 
-const Home: NextPage = () => {
+type Props = {
+  data: GetPostsQuery
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const apolloClient = initializeApollo()
+  const { data } = await apolloClient.query<GetPostsQuery>({
+    query: GET_POSTS,
+    variables: {}
+  })
+  return addApolloState(apolloClient, {
+    props: { data },
+    revalidate: 1
+  })
+}
+
+const Home: VFC<Props> = (props) => {
   return (
     <>
       <Seo path="/" title="Home" description="Homeです" />
       <Layout>
-        <div className="md:max-w-screen-lg md:mx-auto md:pb-12 md:pt-28 md:grid md:grid-cols-article md:gap-[50px]">
-          <div className="md:max-w-[724px] md:col-start-1 md:col-end-2 sm:mx-4">
+        <div className="md:max-w-screen-lg md:mx-auto md:pb-12 md:pt-28 md:flex">
+          <div className="md:max-w-[724px] md:w-full md:col-start-1 md:col-end-2 sm:mx-4 sm:mb-32">
             <h1 className="font-bold text-4xl sm:py-8 md:pb-7">Articles</h1>
-            <div className="grid lg:grid-cols-3 grid-cols-2 md:gap-[30px] sm:gap-4">
-              <Article />
-              <Article />
-              <Article />
-              <Article />
-              <Article />
-              <Article />
+            <div
+              className={cc(['grid grid-cols-article md:gap-[30px] sm:gap-4'])}
+            >
+              {props.data.posts.map((post) => {
+                return (
+                  <Article
+                    key={post.id}
+                    {...post}
+                    // TODO: 1記事以上になったら削除
+                    length={props.data.posts.length}
+                  />
+                )
+              })}
             </div>
           </div>
-          <div className="md:max-w-[250px] md:col-start-2 md:col-end-3 md:pt-[68px] sm:mt-12">
+          <div className="md:max-w-[250px] md:col-start-2 md:col-end-3 md:pt-[68px] md:ml-[50px]">
             <Author />
           </div>
         </div>
