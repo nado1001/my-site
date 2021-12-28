2c9132ba4d43bf4479e21dfe70ecceaaefa94b96
take-tech1001 <tk.tech1001@gmail.com>
2021-12-29 03:00:44 +0900

    💄 Update logo of header

--- src/layout/Header/Header.tsx
+++ src/layout/Header/Header.tsx
@@ -12,7 +12,7 @@ export const Header: VFC = () => {
   const { theme } = useTheme()
 
   return (
-    <header className="bg-white dark:bg-darkBg02 dark:border-darkBorder01 md:dark:border-0 border-border01 border-b px-5 md:w-full z-50">
+    <header className="bg-white dark:bg-darkBg02 dark:border-darkBorder01 md:dark:border-0 border-border01 border-b h-[65px] flex flex-col justify-center md:px-10 sm:px-5 md:w-full z-50">
       <div className="flex items-center justify-between">
         <div>
           <Link href="/">
@@ -24,8 +24,8 @@ export const Header: VFC = () => {
                     : '/image/logo/logo.svg'
                 }
                 alt="nado"
-                width={97}
-                height={72}
+                width={68}
+                height={39}
               />
             </a>
           </Link>
