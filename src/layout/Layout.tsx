e67bed95147edefc7ea597d6244ce5964d15ddf6
take-tech1001 <tk.tech1001@gmail.com>
2022-01-03 03:33:45 +0900

    ♻️ Refactor code

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode, VFC } from 'react'
 
+import { Author } from '../component/Author'
 import { Header } from './Header'
 import { Menu } from './Header'
 
@@ -15,7 +16,16 @@ export const Layout: VFC<Props> = ({ children }) => {
     <div>
       <Header />
       <Menu />
-      <main className="md:mx-[74px]">{children}</main>
+      <main className="md:mx-[74px]">
+        <div className="md:max-w-screen-lg md:mx-auto md:pb-12 md:pt-12 flex sm:flex-col sm:min-h-screen">
+          <div className="md:max-w-[724px] md:w-full md:col-start-1 md:col-end-2 sm:mx-4 sm:mb-32 sm:flex-1">
+            {children}
+          </div>
+          <aside className="md:max-w-[250px] md:col-start-2 md:col-end-3 md:pt-[68px] md:ml-[50px]">
+            <Author />
+          </aside>
+        </div>
+      </main>
     </div>
   )
 }
--- src/pages/index.page.tsx
+++ src/pages/index.page.tsx
@@ -7,7 +7,6 @@ import { addApolloState, initializeApollo } from '../apollo/apolloClient'
 import type { GetPostsQuery } from '../apollo/graphql'
 import { GET_POSTS } from '../apollo/queries'
 import { Article } from '../component/Article'
-import { Author } from '../component/Author'
 import { Seo } from '../component/Seo'
 import { Layout } from '../layout'
 
@@ -37,20 +36,11 @@ const Home: VFC<Props> = (props) => {
         description="ナドの個人ブログ"
       />
       <Layout>
-        <div className="md:max-w-screen-lg md:mx-auto md:pb-12 md:pt-12 md:flex">
-          <div className="md:max-w-[724px] md:w-full md:col-start-1 md:col-end-2 sm:mx-4 sm:mb-32">
-            <h1 className="font-bold text-4xl sm:py-8 md:pb-7">Articles</h1>
-            <div
-              className={cc(['grid grid-cols-article md:gap-[30px] sm:gap-4'])}
-            >
-              {props.data.posts.map((post) => {
-                return <Article key={post.id} {...post} />
-              })}
-            </div>
-          </div>
-          <div className="md:max-w-[250px] md:col-start-2 md:col-end-3 md:pt-[68px] md:ml-[50px]">
-            <Author />
-          </div>
+        <h1 className="font-bold text-4xl sm:py-8 md:pb-7">Articles</h1>
+        <div className={cc(['grid grid-cols-article md:gap-[30px] sm:gap-4'])}>
+          {props.data.posts.map((post) => {
+            return <Article key={post.id} {...post} />
+          })}
         </div>
       </Layout>
     </>
