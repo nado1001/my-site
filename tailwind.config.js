84da29e3ed17b8a53d7949a33e333e9f234bb593
take-tech1001 <tk.tech1001@gmail.com>
2022-01-03 01:29:19 +0900

    🔧 Update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
       },
       screens: { sm: { max: '767px' } },
       gridTemplateColumns: {
-        article: 'repeat(auto-fit, minmax(160px, 1fr))'
+        article: 'repeat(auto-fill, minmax(160px, 1fr))'
       }
     }
   },
