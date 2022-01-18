342a948dc0667641ded411cf00354331e93dc9bf
take-tech1001 <tk.tech1001@gmail.com>
2022-01-18 11:44:51 +0900

    ✨ Add store

---
+++ src/states/store.ts
@@ -0,0 +1,36 @@
+import { proxy, useSnapshot } from 'valtio'
+
+type Store = {
+  menu: boolean
+  tableOfContent: boolean
+}
+
+export const store = proxy<Store>({
+  menu: false,
+  tableOfContent: false
+})
+
+export const actions = {
+  toggleMenu() {
+    store.menu = !store.menu
+  },
+  toggleTableOfContent() {
+    store.tableOfContent = !store.tableOfContent
+  }
+}
+
+export const useMenuState = () => {
+  const snapShot = useSnapshot(store)
+  return {
+    open: snapShot.menu,
+    setOpen: actions.toggleMenu
+  }
+}
+
+export const useTableOfContentState = () => {
+  const snapShot = useSnapshot(store)
+  return {
+    open: snapShot.tableOfContent,
+    setOpen: actions.toggleTableOfContent
+  }
+}
