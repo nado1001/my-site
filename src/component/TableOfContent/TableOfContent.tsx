c0b8af4ed616bfe4fa20a8e1f1f1a08e0fcda23b
take-tech1001 <tk.tech1001@gmail.com>
2022-01-21 15:42:56 +0900

    💄 Update style

--- src/component/TableOfContent/TableOfContent.tsx
+++ src/component/TableOfContent/TableOfContent.tsx
@@ -25,7 +25,7 @@ export const TableOfContent: VFC<Props> = (props) => {
 
   return (
     <div className={cc([`${props.className} md:sticky md:top-8`])}>
-      <div className="bg-white dark:bg-darkBg03 border dark:border-darkBorder01 rounded-lg px-5 pt-5 pb-10 shadow-md sm:absolute sm:top-0 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:w-[calc(100%-100px)]">
+      <div className="bg-white dark:bg-darkBg03 border dark:border-darkBorder01 rounded-lg px-5 pt-5 pb-10 shadow-md sm:absolute sm:top-4 sm:left-1/2 sm:transform sm:-translate-x-1/2 sm:w-[calc(100%-100px)]">
         <span className="sm:hidden block font-bold text-xl">目次</span>
         <ul
           className="mt-5 list-inside leading-8"
