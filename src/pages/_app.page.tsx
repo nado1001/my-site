114690d8c92856c6b383d175bd544b01dfa620aa
take-tech1001 <tk.tech1001@gmail.com>
2022-02-06 15:27:44 +0900

    ✨ Add google analytics

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -2,16 +2,22 @@ import '../styles/globals.css'
 
 import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
 
 import { useApollo } from '../apollo/apolloClient'
+import { GoogleAnalytics, usePageView } from '../lib/gtag'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps)
+  usePageView()
 
   return (
     <ApolloProvider client={apolloClient}>
       <ThemeProvider attribute="class" defaultTheme="dark">
+        <Head>
+          <GoogleAnalytics />
+        </Head>
         <Component {...pageProps} />
       </ThemeProvider>
     </ApolloProvider>
