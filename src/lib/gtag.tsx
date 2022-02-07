8c1fa18808b99de14ab683abb6e2d0129586a4f8
take-tech1001 <tk.tech1001@gmail.com>
2022-02-07 12:22:38 +0900

    ♻️ Refactor gtag

--- src/lib/gtag.tsx
+++ src/lib/gtag.tsx
@@ -1,12 +1,12 @@
 import { useRouter } from 'next/router'
-import Script from 'next/script'
+// import Script from 'next/script'
 import { useEffect } from 'react'
 
 export const GA4_TRACKING_ID = process.env.NEXT_PUBLIC_GA4_ID
 export const UA_TRACKING_ID = process.env.NEXT_PUBLIC_UA_ID
 
 export const pageview = (url: string) => {
-  if (UA_TRACKING_ID && window.gtag) {
+  if (UA_TRACKING_ID) {
     window.gtag('config', UA_TRACKING_ID, {
       page_path: url
     })
@@ -31,17 +31,15 @@ export const usePageView = () => {
 export const GoogleAnalytics = () => (
   <>
     {/* Global site tag (gtag.js) - Google Analytics */}
-    <Script
-      strategy="afterInteractive"
+    <script
+      async
       src={`https://www.googletagmanager.com/gtag/js?id=${GA4_TRACKING_ID}`}
     />
-    <Script
-      strategy="afterInteractive"
+    <script
+      async
       src={`https://www.googletagmanager.com/gtag/js?id=${UA_TRACKING_ID}`}
     />
-    <Script
-      id="gtag-init"
-      strategy="afterInteractive"
+    <script
       dangerouslySetInnerHTML={{
         __html: `
           window.dataLayer = window.dataLayer || [];
