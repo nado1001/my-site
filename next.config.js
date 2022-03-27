e6db6d3102a85ae5124ec591f5d8cb5882c1db17
take-tech1001 <tk.tech1001@gmail.com>
2022-03-27 23:25:46 +0900

    🔧

--- next.config.js
+++ next.config.js
@@ -1,8 +1,13 @@
+const isProd = process.env.NODE_ENV === 'production'
+
 /** @type {import('next').NextConfig} */
 module.exports = {
   future: { strictPostcssConfiguration: true },
   pageExtensions: ['page.tsx', 'page.ts'],
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
-  poweredByHeader: false
+  poweredByHeader: false,
+  env: {
+    stage: isProd ? 'PUBLISHED' : 'DRAFT'
+  }
 }
