48004e63c50343d6074615a16e9746d116a9a064
nado1001 <tk.tech1001@gmail.com>
2022-06-25 23:52:47 +0900

    🐛

--- src/component/ThemeChange/ThemeChange.tsx
+++ src/component/ThemeChange/ThemeChange.tsx
@@ -1,4 +1,4 @@
-import { Switch } from '@headlessui/react'
+import { Switch, useMantineTheme } from '@mantine/core'
 import { useTheme } from 'next-themes'
 import type { FC } from 'react'
 import { useState } from 'react'
@@ -9,6 +9,7 @@ import { useIsomorphicEffect } from '../../hooks'
  * @package
  */
 export const ThemeChange: FC = () => {
+  const mantineTheme = useMantineTheme()
   const { theme, setTheme } = useTheme()
   const [enabled, setEnabled] = useState(false)
 
@@ -23,7 +24,7 @@ export const ThemeChange: FC = () => {
   }
 
   useIsomorphicEffect(() => {
-    if (theme === 'light') {
+    if (theme === 'dark') {
       setEnabled(true)
     }
   }, [])
@@ -33,17 +34,10 @@ export const ThemeChange: FC = () => {
       <Switch
         checked={enabled}
         onChange={handleThemeChange}
-        className={`${
-          enabled ? 'bg-border01' : 'bg-darkBg01'
-        } relative inline-flex flex-shrink-0 h-[28px] w-[60px] border-2 border-transparent rounded-full cursor-pointer transition-crs ease-in-out duration-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
-      >
-        <span
-          aria-hidden="true"
-          className={`${
-            enabled ? 'translate-x-8' : 'translate-x-0'
-          } pointer-events-none inline-block h-[24px] w-[24px] rounded-full bg-white shadow-lg transform ring-0 transition ease-in-out duration-200`}
-        />
-      </Switch>
+        color={mantineTheme.colors.dark[1]}
+        size="lg"
+        defaultChecked={!enabled}
+      />
     </div>
   )
 }
