8fdd21b8e1734054938bf360b4f5b1cc9d6ec621
nado1001 <takeda.k@tsunagu-grp.work>
2022-11-10 20:45:11 +0900

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@ const isProd = process.env.NODE_ENV === 'production'
 
 /** @type {import('next').NextConfig} */
 module.exports = {
-  future: { strictPostcssConfiguration: true },
-  pageExtensions: ['page.tsx', 'page.ts'],
+  // pageExtensions: ['page.tsx', 'page.ts'],
   reactStrictMode: true,
   typescript: { ignoreBuildErrors: true },
   poweredByHeader: false,
   env: {
     stage: isProd ? 'PUBLISHED' : 'DRAFT'
+  },
+  experimental: {
+    appDir: true
   }
 }
