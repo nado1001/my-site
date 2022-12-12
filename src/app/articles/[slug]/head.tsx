f1eefe89cffd784f533909297d4d8c1bbf92a768
nado1001 <tk.tech1001@gmail.com>
2022-12-13 06:07:08 +0900

    refactor: add cache api

--- src/app/articles/[slug]/head.tsx
+++ src/app/articles/[slug]/head.tsx
@@ -1,9 +1,11 @@
+import { cache } from 'react'
+
 import { addApolloState, initializeApollo } from '@/apollo/apolloClient'
 import { GetPostQuery } from '@/apollo/graphql'
 import { GET_POST } from '@/apollo/queries'
 import { Seo } from '@/component/Seo'
 
-const getPost = async (slug: string): Promise<{ data: GetPostQuery }> => {
+const getPost = cache(async (slug: string): Promise<{ data: GetPostQuery }> => {
   const apolloClient = initializeApollo()
   const { data } = await apolloClient.query<GetPostQuery>({
     query: GET_POST,
@@ -16,32 +18,19 @@ const getPost = async (slug: string): Promise<{ data: GetPostQuery }> => {
   return addApolloState(apolloClient, {
     data
   })
-}
+})
+
 export default async function Head({ params }: { params: { slug: string } }) {
   const { data } = await getPost(params.slug)
 
   return (
-    <>
-      <Seo
-        path={data.post?.slug ?? ''}
-        title={data.post?.title ?? ''}
-        description={data.post?.description ?? ''}
-        ogImage={`https://res.cloudinary.com/nado2022112/image/upload/l_text:Sawarabi%20Gothic_80_bold:${encodeURI(
-          data.post?.title ?? ''
-        )},co_rgb:333,w_1000,c_fit/v1641924252/article-ogp_keuplf.png`}
-      />
-      <title>{data.post?.title}</title>
-      <meta name="description" content={data.post?.description ?? ''} />
-      <meta property="og:title" content={data.post?.title} />
-      <meta property="og:description" content={data.post?.description ?? ''} />
-      <meta property="og:type" content="article" />
-      <meta
-        property="og:url"
-        content={`https://res.cloudinary.com/nado2022112/image/upload/l_text:Sawarabi%20Gothic_80_bold:${encodeURI(
-          data.post?.title ?? ''
-        )},co_rgb:333,w_1000,c_fit/v1641924252/article-ogp_keuplf.png`}
-      />
-      <meta />
-    </>
+    <Seo
+      path={data.post?.slug ?? ''}
+      title={data.post?.title ?? ''}
+      description={data.post?.description ?? ''}
+      ogImage={`https://res.cloudinary.com/nado2022112/image/upload/l_text:Sawarabi%20Gothic_80_bold:${encodeURI(
+        data.post?.title ?? ''
+      )},co_rgb:333,w_1000,c_fit/v1641924252/article-ogp_keuplf.png`}
+    />
   )
 }
--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -1,10 +1,10 @@
 import cc from 'classcat'
+import { cache } from 'react'
 
 import { addApolloState, initializeApollo } from '@/apollo/apolloClient'
 import type { GetPostsByTagNameQuery, GetTagsQuery } from '@/apollo/graphql'
 import { GET_POSTS_BY_TAG_NAME, GET_TAGS } from '@/apollo/queries'
 import { Article } from '@/component/Article'
-import { Seo } from '@/component/Seo'
 
 type Props = {
   data: GetPostsByTagNameQuery
@@ -26,7 +26,7 @@ export async function generateStaticParams() {
   })
 }
 
-const getTags = async (tag: string): Promise<Props> => {
+const getTags = cache(async (tag: string): Promise<Props> => {
   const apolloClient = initializeApollo()
   const { data } = await apolloClient.query<GetPostsByTagNameQuery>({
     query: GET_POSTS_BY_TAG_NAME,
@@ -39,19 +39,13 @@ const getTags = async (tag: string): Promise<Props> => {
   return addApolloState(apolloClient, {
     data
   })
-}
+})
 
 export default async function Tags({ params }: { params: { tag: string } }) {
   const { data } = await getTags(params.tag)
 
   return (
     <>
-      <Seo
-        path={`/tags/${data.tags[0].tagSlug}`}
-        noTitleTemplate={true}
-        title={`${data.tags[0].tagName}の記事一覧 | nado`}
-        description="ナドの個人ブログ"
-      />
       <h1 className="font-bold text-4xl sm:py-8 md:pb-7">
         {data.tags[0].tagName}
       </h1>
