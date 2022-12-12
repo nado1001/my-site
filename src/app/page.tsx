608dd1ca3dd0217ac4816a62076fdc1a0addb31b
nado1001 <tk.tech1001@gmail.com>
2022-12-13 07:05:30 +0900

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import cc from 'classcat'
 
-import { initializeApollo } from '@/apollo/apolloClient'
+import { addApolloState, initializeApollo } from '@/apollo/apolloClient'
 import type { GetPostsQuery } from '@/apollo/graphql'
 import { GET_POSTS } from '@/apollo/queries'
 import { Article } from '@/component/Article'
-import { Seo } from '@/component/Seo'
 import { generatedRssFeed } from '@/lib/feed'
 
 import { DefaultLayout } from './DefaultLayout'
@@ -24,7 +23,9 @@ const getPosts = async (): Promise<Props> => {
 
   await generatedRssFeed(data)
 
-  return { data: data }
+  return addApolloState(apolloClient, {
+    data
+  })
 }
 
 export default async function Page() {
@@ -33,21 +34,13 @@ export default async function Page() {
   } = await getPosts()
 
   return (
-    <>
-      <Seo
-        path=""
-        noTitleTemplate={true}
-        title="nado"
-        description="ナドの個人ブログ"
-      />
-      <DefaultLayout>
-        <h1 className="font-bold text-4xl sm:py-8 md:pb-7">Articles</h1>
-        <div className={cc(['grid grid-cols-article md:gap-[30px] sm:gap-4'])}>
-          {posts.map((post) => {
-            return <Article key={post.id} {...post} />
-          })}
-        </div>
-      </DefaultLayout>
-    </>
+    <DefaultLayout>
+      <h1 className="font-bold text-4xl sm:py-8 md:pb-7">Articles</h1>
+      <div className={cc(['grid grid-cols-article md:gap-[30px] sm:gap-4'])}>
+        {posts.map((post) => {
+          return <Article key={post.id} {...post} />
+        })}
+      </div>
+    </DefaultLayout>
   )
 }
