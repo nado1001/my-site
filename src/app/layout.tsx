60a678e4d8566be6fa5165e2971cba9da8e87dc5
nado1001 <tk.tech1001@gmail.com>
2023-01-31 01:05:29 +0900

    fix: fix layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,33 +1,33 @@
-'use client'
+// 'use client'
 
 import '@/styles/globals.css'
 
-import { ApolloProvider } from '@apollo/client'
-import { ThemeProvider } from 'next-themes'
+// import { ApolloProvider } from '@apollo/client'
+// import { ThemeProvider } from 'next-themes'
 import React from 'react'
 
-import { useApollo } from '@/apollo/apolloClient'
-import { GoogleAnalytics, usePageView } from '@/lib/gtag'
+// import { useApollo } from '@/apollo/apolloClient'
+import { GoogleAnalytics } from '@/lib/gtag'
 
 export default function RootLayout({
   children
 }: {
   children: React.ReactNode
 }) {
-  const apolloClient = useApollo(children)
-  usePageView()
+  // const apolloClient = useApollo(children)
+  // usePageView()
 
   return (
-    <html lang="ja" className="sm:scroll-pt-[50px] scroll-smooth">
+    <html lang="ja" className="sm:scroll-pt-[50px] scroll-smooth dark">
       <head>
         <GoogleAnalytics />
       </head>
       <body>
-        <ApolloProvider client={apolloClient}>
-          <ThemeProvider attribute="class" defaultTheme="dark">
-            {children}
-          </ThemeProvider>
-        </ApolloProvider>
+        {/* <ApolloProvider client={apolloClient}> */}
+        {/* <ThemeProvider attribute="class" defaultTheme="dark"> */}
+        {children}
+        {/* </ThemeProvider> */}
+        {/* </ApolloProvider> */}
       </body>
     </html>
   )
