6c53b0c0a6add89359f7069f51eb088c4fbc701b
nado1001 <tk.tech1001@gmail.com>
2023-01-31 00:13:08 +0900

    fix: remove revalidate

--- src/app/articles/[slug]/layout.tsx
+++ src/app/articles/[slug]/layout.tsx
@@ -6,8 +6,6 @@ type Props = {
   children: ReactNode
 }
 
-export const revalidate = 3600
-
 export default function Layout({ children }: Props) {
   return <ArticleLayout>{children}</ArticleLayout>
 }
